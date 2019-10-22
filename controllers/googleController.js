cee6932f848089eb0ccbf6b6e18f253c420b9023
Liz Guenthner <liz.guenthner@t-3.com>
2019-10-21 20:26:06 -0500

    update google link

--- controllers/googleController.js
+++ controllers/googleController.js
@@ -6,7 +6,7 @@ module.exports = {
     getAllBooks: function(req, res) {
         // axios call to find all books in google books search using parameter in query
         axios
-        .get('https://www.googleapis.com/books/v1/volumes?q='+ { params: req.query } + '&printType=books&_limit=10')
+        .get('https://www.googleapis.com/books/v1/volumes?q=' + { params: req.query } + '&printType=books&_limit=10')
         .then(({ data: { results } }) => res.json(results))
         .catch(err => res.status(422).json(err));
     }
